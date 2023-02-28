2cce233057c971f819facc7c2dd171170570d56b
WesleyGuzman77 <125400483+WesleyGuzman77@users.noreply.github.com>
2023-02-28 12:23:23 -0500

    Adding Enemies

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
-    float health = 10f;
+   public float speed = 5f;
+    int health = 10;
+    public TMP_Text healthText;
     int score = 0;
     float thisIsADecimal = 3.14f;
 
@@ -14,6 +17,8 @@ public class PlayerControl : MonoBehaviour
         Debug.Log(thisIsADecimal);
         thisIsADecimal++;
         Debug.Log(thisIsADecimal);
+
+        healthText.text = health.ToString();
     }
 
     // Update is called once per frame
@@ -21,27 +26,42 @@ public class PlayerControl : MonoBehaviour
     {
         //transform.position = new Vector3(12, 2, 15);
 
-        Vector3 newPos = transform.position;
+        Vector3 currentPos = transform.position;
 
 
         if (Input.GetKey(KeyCode.D))
         {
-            newPos.x++;
+            currentPos.x = currentPos.x + speed * Time.deltaTime;
+            //newPos.x++;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            newPos.x--;
+            currentPos.x = currentPos.x - speed * Time.deltaTime;
+            //newPos.x--;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            newPos.z++;
+            currentPos.z = currentPos.z + speed * Time.deltaTime;
+            //newPos.z++;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            newPos.z--;
+            currentPos.z = currentPos.z - speed * Time.deltaTime;
+            //newPos.z--;
         }
 
 
-        transform.position = newPos;
+        transform.position = currentPos;
     }
+
+    void OnCollisionEnter(Collision otherThing)
+    {
+        Debug.Log(otherThing.gameObject.name);
+        if (otherThing.gameObject.name == "Enemy")
+        {
+            health--;
+            healthText.text = health.ToString();
+        }
+    }
+
 }
